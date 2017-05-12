1d6df3f28917fd48a6de325cb5e6d84c789c1da4
LinXiaoBao <920854306@qq.com>
2017-05-12 15:57:40 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<ol>
+        <li v-for='item in groceryList' v-html="item.text"></li>
+    </ol> 
+</template>
+<script>
+export default {
+	data: function(){
+		return {
+			groceryList: [{
+	            text: '<p style="color:red">蔬菜</p>'
+	        }, {
+	            text: '水果'
+	        }, {
+	            text: '其他'
+	        }]
+		}
+	}
+}
+</script>
+<style></style>
---
+++ src/math.js
@@ -0,0 +1,4 @@
+module.exports = {
+	x: 200,
+	y: 300,
+};
